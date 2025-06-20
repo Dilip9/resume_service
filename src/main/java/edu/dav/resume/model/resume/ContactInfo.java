baa6485e96555b72f6bc5af94d68c9155714deb1
Dilip9 <dilip.verma940805@gmail.com>
2025-06-20 16:31:07 +0530

    Contact database design model

--- src/main/java/edu/dav/resume/model/resume/ContactInfo.java
+++ src/main/java/edu/dav/resume/model/resume/ContactInfo.java
@@ -1,4 +1,31 @@
 package edu.dav.resume.model.resume;
 
+import edu.dav.resume.model.enums.ContactType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_contact_info", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"type", "value", "resume_id"})
+})
 public class ContactInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ContactType type;
+
+    private String value;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private U_Resume resume;
 }
