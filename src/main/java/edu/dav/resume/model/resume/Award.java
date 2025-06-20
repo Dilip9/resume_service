e707877644fbfb30a3f69cdca9fdbb09a487cf76
Dilip9 <dilip.verma940805@gmail.com>
2025-06-20 16:22:44 +0530

    Award Database design model

--- src/main/java/edu/dav/resume/model/resume/Award.java
+++ src/main/java/edu/dav/resume/model/resume/Award.java
@@ -1,4 +1,34 @@
 package edu.dav.resume.model.resume;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@Entity
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_awards")
 public class Award {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private LocalDate awardDate;
+
+    @ManyToOne
+    private U_Resume resume;
+
 }
