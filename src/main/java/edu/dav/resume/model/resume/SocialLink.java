75e72bd56dc8cf2734f585ad6548d6a903c8fd16
Dilip9 <dilip.verma940805@gmail.com>
2025-06-20 16:34:09 +0530

    Social media network design model

--- src/main/java/edu/dav/resume/model/resume/SocialLink.java
+++ src/main/java/edu/dav/resume/model/resume/SocialLink.java
@@ -1,4 +1,29 @@
 package edu.dav.resume.model.resume;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_social_links")
 public class SocialLink {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String platfrom;
+
+    private String url;
+
+    @ManyToOne
+    private U_Resume resume;
 }
