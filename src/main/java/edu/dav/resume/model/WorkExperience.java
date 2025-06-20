e9d6dfd3aad6fc57c0a5765efc3941bc7738f1c6
Dilip9 <dilip.verma940805@gmail.com>
2025-06-20 16:16:12 +0530

    Added Work Experience design model class

--- src/main/java/edu/dav/resume/model/WorkExperience.java
+++ src/main/java/edu/dav/resume/model/WorkExperience.java
@@ -1,4 +1,40 @@
 package edu.dav.resume.model;
 
+import edu.dav.resume.model.resume.U_Resume;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.domain.Auditable;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_work_experiences")
 public class WorkExperience {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String jobTitle;
+
+    private String companyName;
+
+    private String location;
+
+    private Boolean active;
+
+    @Column(columnDefinition = "TEXT")
+    private String responsiblity;
+
+    @ManyToOne
+    private U_Resume resume;
+
+    @Embedded
+    private Auditable auditable;
+
 }
